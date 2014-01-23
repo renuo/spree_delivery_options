f5dfeffbdfd6aa604568cb0ad9797aae225cfada
Francisco Trindade <frank.trindade@gmail.com>
2014-01-23 12:40:07 +1100

    updated configuration file

--- lib/spree_delivery_options/configuration.rb
+++ lib/spree_delivery_options/configuration.rb
@@ -1,6 +1,6 @@
 module SpreeDeliveryOptions
   class Configuration < Spree::Preferences::Configuration
     preference :delivery_cut_off_hour, :integer, default: 12
-    preference :delivery_time_options, :string, default: {early_morning: 'Between 6am-8am'}.to_json
+    preference :delivery_time_options, :string, default: {monday: ['Between 6am-8am']}.to_json
   end
 end
