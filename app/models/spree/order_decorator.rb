804cd90d7fdb18c63f669d9d25c9aa395d32343d
ideadapt <kunz@ideadapt.net>
2014-04-08 14:13:29 +0200

    refs #1572, delivery date i18n and layout

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,20 +4,20 @@ Spree::Order.class_eval do
 
   def valid_delivery_instructions?
     if self.delivery_instructions && self.delivery_instructions.length > 500
-      self.errors[:delivery_instructions] << 'cannot be longer than 500 charachters'
+      self.errors[:delivery_instructions] << I18n.t('activerecord.errors.models.spree/order.attributes.delivery_instructions.cannot_be_longer_than_500_charachters')
       return false
     end
     true
   end
 
   def valid_delivery_date?
-    self.errors[:delivery_date] << 'cannot be blank' unless self.delivery_date
+    self.errors[:delivery_date] << I18n.t('activerecord.errors.messages.blank') unless self.delivery_date
     if self.delivery_date
-      self.errors[:delivery_date] << 'cannot be today or in the past' if self.delivery_date <= Date.today
+      self.errors[:delivery_date] << I18n.t('activerecord.errors.models.spree/order.attributes.delivery_date.cannot_be_today_or_in_the_past') if self.delivery_date <= Date.today
 
       cutoff_time = Time.now.change(hour: SpreeDeliveryOptions::Config.delivery_cut_off_hour)
       if self.delivery_date == Date.tomorrow && Time.now > (cutoff_time + 15.minutes)
-        self.errors[:delivery_date] << "cannot be tomorrow if the order is created after 1pm"
+        self.errors[:delivery_date] << I18n.t('activerecord.errors.models.spree/order.attributes.delivery_date.cannot_be_tomorrow_if_the_order_is_created_after', cut_off: cutoff_time.hour)
       end
     end
 
@@ -27,10 +27,10 @@ Spree::Order.class_eval do
   def valid_delivery_time?
     return unless self.delivery_date
 
-    self.errors[:delivery_time] << 'cannot be blank' unless self.delivery_time
+    self.errors[:delivery_time] << I18n.t('activerecord.errors.messages.blank') unless self.delivery_time
 
     if self.delivery_time
-      self.errors[:delivery_time] << 'is invalid' unless (delivery_time_options(self.delivery_date) && delivery_time_options(self.delivery_date).include?(self.delivery_time))
+      self.errors[:delivery_time] << I18n.t('activerecord.errors.messages.invalid') unless (delivery_time_options(self.delivery_date) && delivery_time_options(self.delivery_date).include?(self.delivery_time))
     end
 
     self.errors[:delivery_time].empty? ? true : false
